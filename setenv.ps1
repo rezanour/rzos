bb2920ca2b4207efcbd30b3aaf5aeac0074967e5
Reza Nourai <rezanour@hotmail.com>
2015-08-07 00:34:31 -0700

    Add alias for running qemu.

--- setenv.ps1
+++ setenv.ps1
@@ -1,2 +1,3 @@
 set -name repo_root -value (get-location) -scope "Global"
 set-alias -name fasm -value $repo_root"\build\fasm17139\fasm.exe" -scope "Global"
+set-alias -name qemu -value $repo_root"\tools\qemu\qemu-system-i386.exe" -scope "Global"
