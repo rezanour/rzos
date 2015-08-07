8d4fdc0f04e8f2c51146d378463e9820c654dd2e
Reza Nourai <rezanour@hotmail.com>
2015-08-06 21:55:04 -0700

    Add QEmu to tools directory. Update bootloader to print a simple string so we know it's running.

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,5 +1,14 @@
-boot_start:
-    jmp boot_start
+  mov ah, 0x0e      ; scrolling teletype BIOS routine
+  mov al, 'R'
+  int 0x10
+  mov al, 'e'
+  int 0x10
+  mov al, 'z'
+  int 0x10
+  mov al, 'a'
+  int 0x10
+  
+  jmp $
 
-    rb 510-($-$$)   ; fill up to the 510th byte
-    dw 0xaa55       ; boot loader signature
+  rb 510-($-$$)   ; fill up to the 510th byte
+  dw 0xaa55       ; boot loader signature
