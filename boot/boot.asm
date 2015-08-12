fe96d8a68787b0cb54b0a466ad09aeef3ccb007b
Reza Nourai <rezanour@hotmail.com>
2015-08-11 21:55:20 -0700

    Clean up of the boot sector a bit.

--- boot/boot.asm
+++ boot/boot.asm
@@ -8,6 +8,9 @@
 
 org 0x7c00     ; where BIOS loads us
 
+  mov bx, WELCOME_MSG
+  call print_string
+
   ; BIOS only loads 1 sector into memory and starts execution.
   ; We have code after that, so load the remainder of our boot
   ; loader into memory now.
@@ -26,34 +29,16 @@ org 0x7c00     ; where BIOS loads us
   je no_partial
   add al, 1      ; add 1 if there is a partial sector left
 no_partial:
-
   ; al now has # of sectors to load
-  mov bx, 0      ; 0 offset from es
 
+  mov bx, 0      ; 0 offset from es
   mov ah, 0x02   ; read sectors from drive
   mov dl, 0x80   ; first hdd
   mov dh, 0      ; head
   mov ch, 0      ; cylinder
-  mov cl, 2
+  mov cl, 2      ; start sector, BIOS already read 1 into mem
   int 0x13       ; disk service routine
-  ;pushw ax       ; count
-  ;pushw 2        ; start sector, 1 is the one BIOS already loaded
-  ;call read_sectors
   jc boot_error
-
-  ; change to a graphics mode
-  call set_graphics_mode
-
-  pushw 3          ; background color
-  call clear_screen
-
-  ; draw a simple rectangle
-  pushw 1   ; color
-  pushw 50  ; height
-  pushw 200 ; width
-  pushw 10  ; y
-  pushw 60  ; x
-  call draw_rect
 
   jmp the_end
 
@@ -61,28 +46,59 @@ no_partial:
   ; text and then displaying a simple 'x'
 
 boot_error:
-  call set_default_mode
-
-  mov ah, 0x0e    ; teletype
-  mov al, 'x'
-  int 10h
-
+  mov bx, ERROR_MSG
+  call print_string
   jmp the_end
 
 the_end:
   jmp $           ; hang, nothing else to do
 
-  ; BIOS detects boot loader by looking for a signature
-  ; at the end of the first 512 byte sector of the drive.
-  ; fill out the rest of our first sector (with 0) and then
-  ; add the magic signature
+;**********************************************************
+; boot sector data section. All data & routines used before
+; loading remainder of loader must fit in first 512 bytes.
+;**********************************************************
+
+WELCOME_MSG     db 'Welcome to RZOS, by Reza Nourai.', 0x0d, 0x0a, 0
+ERROR_MSG       db 'Error occurred loading remainder of boot loader into memory.', 0
+
+;**********************************************************
+; print_string - Prints null-terminated string at offset BX
+; in: BX = offset of string
+; out: none
+;**********************************************************
+print_string:
+  push ax
+  push bx
+  mov ah, 0x0e        ; teletype routine
+@@:
+  mov al, byte ptr bx ; read character from string
+  cmp al, 0           ; if 0..
+  je @f               ;    .. done
+  int 10h             ; invoke BIOS video service
+  inc bx              ; advance pointer
+  jmp @b
+@@:
+  pop bx
+  pop ax
+  ret
+
+;**********************************************************
+; BIOS detects boot loader by looking for a signature
+; at the end of the first 512 byte sector of the drive.
+; fill out the rest of our first sector (with 0) and then
+; add the magic signature
+;**********************************************************
  
   rb 510-($-$$)   ; fill up to the 510th byte
   dw 0xaa55       ; boot loader signature
 
 end_of_boot_sector:
 
-include '..\filesystem\disk.asm'
-include '..\graphics\graphics.asm'
+;**********************************************************
+; Load remaining boot loader code in sectors after.
+; These will all get fetched into mem when we complete the 
+; multi-sector read above.
+;**********************************************************
+include '../graphics/graphics.asm'
 
 end_of_boot_loader:
