ed560f033e694e5b77a07deb8a8958594d855496
Reza Nourai <rezanour@hotmail.com>
2015-08-20 23:12:46 -0700

    Load system memory information and RAM layout. New 0-padded hex printing support for improving readability of large number display.

--- boot/boot.asm
+++ boot/boot.asm
@@ -24,11 +24,7 @@ use16           ; use 16-bit real mode (what CPU starts in on boot)
 
   mov bx, WELCOME_MSG
   call print_string
-  mov bx, NEWLINE
-  call print_string
-
-  mov bx, 13234
-  call print_number
+  call print_newline
 
   ; compute remaining size in # of 512 byte sectors.
   rest_of_boot_loader = end_of_boot_loader - end_of_boot_sector
@@ -50,18 +46,31 @@ use16           ; use 16-bit real mode (what CPU starts in on boot)
   mov ch, 0       ; cylinder 0
   mov cl, 2       ; start sector, BIOS already read 1 into mem
   int 0x13        ; invoke disk service routine
-  jc boot_error
+  jc .boot_error
 
-  jmp the_end
+  ; load up the memory map
+  call load_mem_map
+  jc .mem_error
 
-boot_error:
-  mov bx, LOAD_ERROR_MSG
+  ; print memory summary
+  call print_mem_map
+
+  jmp .the_end
+
+.boot_error:
+  mov dx, LOAD_ERROR_MSG
+  jmp .error
+
+.mem_error:
+  mov dx, MEM_ERROR_MSG
+  jmp .error
+
+.error:
   call print_string
-  mov bx, NEWLINE
-  call print_string
-  jmp the_end
+  call print_newline
+  jmp .the_end
 
-the_end:
+.the_end:
   mov bx, END_MSG
   call print_string
   jmp $           ; hang, nothing else to do
@@ -71,7 +80,8 @@ the_end:
 ;**********************************************************
 
 WELCOME_MSG     db 'Welcome to RZOS, by Reza Nourai.', 0
-LOAD_ERROR_MSG  db 'Error occurred loading remainder of boot loader into memory.', 0
+LOAD_ERROR_MSG  db 'Error loading remainder of boot loader into memory.', 0
+MEM_ERROR_MSG   db 'Error querying system memory information.', 0
 END_MSG         db 'The End.', 0
 NEWLINE         db 0x0d, 0x0a, 0
 
@@ -94,6 +104,18 @@ print_string:
 @@:
   pop bx
   pop ax
+  ret
+
+;**********************************************************
+; print_newline - Prints newline
+; in: none
+; out: none
+;**********************************************************
+print_newline:
+  push bx
+  mov  bx, NEWLINE
+  call print_string
+  pop bx
   ret
 
 ;**********************************************************
@@ -134,6 +156,52 @@ print_number:
   ret
 
 ;**********************************************************
+; print_hex - Prints number stored in BX as hex with leading 0s
+; in: BX = an integer
+; out: none
+;**********************************************************
+print_hex:
+  push ax
+  push bx
+  push cx
+  push dx
+  push 0xffff     ; insert token to mark end of digits in stack
+  mov cx, 16
+  mov ax, bx
+  mov bx, 4       ; num iterations (so we force 0s)
+@@:
+  xor dx, dx      ; dx (upper word of dividend) to 0
+  div cx          ; divide by 10 to extract digit
+  push dx         ; store remainder as our digit
+  dec bx
+  cmp bx, 0       ; if count = 0, we're done
+  je @f
+  jmp @b
+@@:
+  mov ah, 0x0e    ; select teletype routine
+@@:
+  pop bx          ; pull out a digit (in high byte)
+  cmp bx, 0xffff  ; first, check for our end marker
+  je @f
+  mov al, bl      ; move quotient to al so we can use it for call
+  cmp al, 10
+  jge .letter
+  add al, '0'     ; add ascii offset for digit
+  int 10h         ; invoke BIOS video service
+  jmp @b
+.letter:
+  sub al, 10
+  add al, 'A'     ; add ascii offset for digit
+  int 10h         ; invoke BIOS video service
+  jmp @b
+@@:
+  pop dx
+  pop cx
+  pop bx
+  pop ax
+  ret
+
+;**********************************************************
 ; BIOS detects boot loader by looking for a signature
 ; at the end of the first 512 byte sector of the drive.
 ; fill out the rest of our first sector (with 0) and then
@@ -145,13 +213,11 @@ print_number:
 
 end_of_boot_sector:
 
-DUMMY     db 1    ; ensure there is at least something in the next sector
-
 ;**********************************************************
 ; Load remaining boot loader code in sectors after.
 ; These will all get fetched into mem when we complete the 
 ; multi-sector read above.
 ;**********************************************************
-;include '../graphics/graphics.asm'
+include 'memory.asm'
 
 end_of_boot_loader:
---
+++ boot/memory.asm
@@ -0,0 +1,175 @@
+;**********************************************************
+; boot/memory.asm
+; RZOS boot loader memory support
+;
+; RZOS 2015/08/20
+; Reza Nourai
+;**********************************************************
+
+;**********************************************************
+; data
+;**********************************************************
+
+; the memory map is a set of 24 byte entries describing:
+;     start offset of segment (8 bytes)
+;     size of segment in bytes (8 bytes)
+;     type of segment (4 bytes)
+;         1 = usable RAM
+;         2 = Reserved (unusable)
+;         3 = ACPI reclaimable
+;         4 = ACPI nonreclaimable
+;         5 = Identified bad memory
+;     ACPI 3.0 extended attributes bitfield (4 bytes)
+num_mmap_entries  dw  0
+mmap_entry        db  0 dup(240)  ; each entry is 24 bytes, so reserve room for 10 entries
+
+BYTES_OF        db ' bytes of ', 0
+AVAILABLE       db 'available memory', 0
+RESERVED        db 'reserved memory', 0
+OTHERMEM        db 'other memory', 0
+
+;**********************************************************
+; load_mem_map - Load system memory map
+; in: none
+; out: none
+;**********************************************************
+load_mem_map:
+  push eax
+  push ebx
+  push ecx
+  push edx
+  push bp
+  push es
+  push di
+
+  mov ax, mmap_entry
+  mov es, ax            ; set our segment pointer to point to the start of our entries
+  mov di, 0
+  xor ebx, ebx          ; 0 out ebx
+  xor bp, bp            ; 0 out bp. It'll hold our rolling count
+  mov edx, 0x0534D4150  ; "SMAP" parameter for querying system memory map
+  mov eax, 0xe820       ; query system memory map function of BIOS
+  mov ecx, 24           ; ask for full 24 byte entries
+  mov [es:di + 20], dword 1   ; set ACPI 3.x field to 1 (query ext info if avail)
+  int 15h               ; invoke BIOS function
+  jc .failed            ; carry on the first call means unsupported
+  mov edx, 0x0534D4150  ; apparently some BIOS's trash this register during the call so fix it
+  cmp eax, edx          ; on success, eax must have been set to "SMAP"
+  jne .failed
+  test ebx, ebx         ; ebx must be non zero
+  je .failed
+  jmp .process_entry
+
+.query_entry:
+  mov eax, 0xe820       ; eax, ecx get trashed on each call so set them up again
+  mov ecx, 24           ; ask for full 24 byte entries
+  mov [es:di + 20], dword 1   ; set ACPI 3.x field to 1 (query ext info if avail)
+  int 15h               ; invoke BIOS function
+  jc .end_of_list       ; carry means "end of list"
+  mov edx, 0x0534D4150  ; restore register in case BIOS trashed it
+
+.process_entry:
+  jcxz .skip            ; skip 0 length entries
+  cmp cl, 20            ; we asked for 24, did we only get 20 byte entry?
+  jbe .noext            ; no extended data
+  test byte [es:di + 20], 1   ; extended data with 1 set means 'ignore'
+  je .skip
+
+.noext:
+  mov ecx, [es:di + 8]  ; get lower dword of length
+  or ecx, [es:di + 12]  ; or with upper to check for 0
+  jz .skip
+  inc bp                ; got a good entry, so ++count
+  add di, 24            ; and move our dest pointer to next entry
+
+.skip:
+  test ebx, ebx         ; if ebx is 0, list complete
+  jne .query_entry
+
+.end_of_list:
+  mov [num_mmap_entries], bp  ; store off the total count
+  clc                   ; clear carry flag if set
+  jmp .end
+
+.failed:
+  stc                   ; set carry flag to mean error
+
+.end:
+  pop di
+  pop es
+  pop bp
+  pop edx
+  pop ecx
+  pop ebx
+  pop eax
+  ret
+
+;**********************************************************
+; print_mem_map - Print system memory map info
+; in: none
+; out: none
+;**********************************************************
+print_mem_map:
+  push ax
+  push bx
+  push cx
+  push es
+  push di
+
+  mov cx, [num_mmap_entries]
+  mov ax, mmap_entry
+  mov es, ax
+  mov di, 0
+
+.next_entry:
+  test cx, cx           ; any entries left?
+  jz .end
+
+  dec cx
+
+  mov bx, word [es:di + 14]
+  call print_hex
+  mov bx, word [es:di + 12]
+  call print_hex
+  mov bx, word [es:di + 10]
+  call print_hex
+  mov bx, word [es:di + 8]
+  call print_hex
+  mov bx, BYTES_OF
+  call print_string
+
+  mov ax, word [es:di + 16] ; get type
+  cmp ax, 1             ; avail
+  je .avail
+  cmp ax, 2             ; reserved
+  je .reserved
+  jmp .other
+
+.avail:
+  mov bx, AVAILABLE
+  call print_string
+  call print_newline
+  add di, 24
+  jmp .next_entry
+
+.reserved:
+  mov bx, RESERVED
+  call print_string
+  call print_newline
+  add di, 24
+  jmp .next_entry
+
+.other:
+  mov bx, OTHERMEM
+  call print_string
+  call print_newline
+  add di, 24
+  jmp .next_entry
+
+.end:
+  pop di
+  pop es
+  pop cx
+  pop bx
+  pop ax
+  ret
